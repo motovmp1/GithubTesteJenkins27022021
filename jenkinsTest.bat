189bd1bbc8fcb3f54dac5d56772da5f3d4899725
motovmp1 <vinicius.mpinho@gmail.com>
2021-02-27 11:13:24 +0000

    third commit

--- jenkinsTest.bat
+++ jenkinsTest.bat
@@ -1,6 +1,6 @@
 echo off
 cls
-echo "SCRIPT .BAT para realizar GitHub Teste 2"
+echo "SCRIPT .BAT para realizar GitHub Teste 3 em git"
 ping 127.0.0.1 -n 15 > nul
 
 cd C:\Users\User01\Documents\JenkinsFolderTesteCurso2021\
