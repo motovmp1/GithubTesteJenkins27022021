702e7a290407c3785d40ca0a009dbab1433cc64c
motovmp1 <vinicius.mpinho@gmail.com>
2021-02-27 11:04:10 +0000

    second commit

--- jenkinsTest.bat
+++ jenkinsTest.bat
@@ -1,6 +1,6 @@
 echo off
 cls
-echo "SCRIPT .BAT para realizar GitHub Teste"
+echo "SCRIPT .BAT para realizar GitHub Teste 2"
 ping 127.0.0.1 -n 15 > nul
 
 cd C:\Users\User01\Documents\JenkinsFolderTesteCurso2021\
